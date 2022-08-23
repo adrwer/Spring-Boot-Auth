82c9d6ac135fe44d483752a6579cb4b38f591dc9
Adrian Werimo <adrwer7@gmail.com>
2022-08-23 16:41:53 +0300

    Project and AppUser Setup

---
+++ src/main/java/com/example/demo/appuser/AppUserService.java
@@ -0,0 +1,21 @@
+package com.example.demo.appuser;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AppUserService implements UserDetailsService {
+
+    private  final static String USER_NOT_FOUND = "User with username %s not found.";
+    private final AppUserRepository appUserRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return appUserRepository.findByEmail(username).orElseThrow(() ->
+                new UsernameNotFoundException(String.format(USER_NOT_FOUND, username)));
+    }
+}
